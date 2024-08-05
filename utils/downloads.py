cd56a7506334eb162f02992565301cbea7cba5ca
zhangbiao1231 <18856307989@163.com>
2024-08-05 20:53:53 +0800

    addmodified:utils/downloads.py

---
+++ utils/downloads.py
@@ -0,0 +1,106 @@
+import collections
+import hashlib
+import os
+import math
+import tarfile
+import zipfile
+import requests
+import shutil
+
+#@save
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+def download(name, cache_dir=os.path.join('../dog-breed', 'data_test')):  #@save
+    """下载一个DATA_HUB中的文件，返回本地文件名"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # 命中缓存
+    print(f'正在从{url}下载{fname}...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+def extract(fname, folder=None):  #@save
+    """下载并解压zip/tar文件"""
+    # fname = download(name)
+    base_dir = os.path.dirname(fname)
+    # print(f'base_dir:{base_dir}')
+    data_dir, ext = os.path.splitext(fname)
+    # print(f'data_dir:{data_dir}')
+    if ext == '.zip':
+        fp = zipfile.ZipFile(fname, 'r')
+    elif ext in ('.tar', '.gz'):
+        fp = tarfile.open(fname, 'r')
+    else:
+        assert False, f'只有zip/tar文件可以被解压缩'
+    fp.extractall(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+def read_csv_labels(fname):
+    """读取fname来给标签字典返回一个文件名"""
+    with open(fname, 'r') as f:
+        # 跳过文件头行(列名)
+        lines = f.readlines()[1:]
+    tokens = [l.rstrip().split(',') for l in lines]
+    return dict(((name, label) for name, label in tokens))
+def copyfile(filename, target_dir):
+    """将文件复制到目标目录"""
+    os.makedirs(target_dir, exist_ok=True)
+    shutil.copy(filename, target_dir)
+
+#@save
+def reorg_train_valid(data_dir, labels, valid_ratio):
+    """将验证集从原始的训练集中拆分出来"""
+    # 训练数据集中样本最少的类别中的样本数
+    n = collections.Counter(labels.values()).most_common()[-1][1]
+    # 验证集中每个类别的样本数
+    n_valid_per_label = max(1, math.floor(n * valid_ratio))
+    label_count = {}
+    for train_file in os.listdir(os.path.join(data_dir, 'train')):
+        label = labels[train_file.split('.')[0]]
+        fname = os.path.join(data_dir, 'train', train_file)
+        copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                     'train_valid', label))
+        if label not in label_count or label_count[label] < n_valid_per_label:
+            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                         'valid', label))
+            label_count[label] = label_count.get(label, 0) + 1
+        else:
+            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                         'train', label))
+    return n_valid_per_label
+#@save
+def reorg_test(data_dir):
+    """在预测期间整理测试集，以方便读取"""
+    for test_file in os.listdir(os.path.join(data_dir, 'test')):
+        copyfile(os.path.join(data_dir, 'test', test_file),
+                 os.path.join(data_dir, 'train_valid_test', 'test',
+                              'unknown'))
+def reorg_dog_data(data_dir, valid_ratio):
+    labels = read_csv_labels(os.path.join(data_dir, 'labels.csv'))
+    reorg_train_valid(data_dir, labels, valid_ratio)
+    reorg_test(data_dir)
+
+if __name__ == '__main__':
+    fname = '../dog-breed/data/dog-breed-identification.zip'
+    data_dir = os.path.dirname(fname)
+    # print(data_dir)
+    extract(fname)
+    valid_ratio = 0.1
+    reorg_dog_data(data_dir,valid_ratio)
+    labels = read_csv_labels(os.path.join(data_dir, 'labels.csv'))
+    print('# 训练样本 :', len(labels))
+    print('# 类别 :', len(set(labels.values())))
+
