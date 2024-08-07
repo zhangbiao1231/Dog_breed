3af47782ea01467999ad65be1910c40fe84c50ac
Zebulon <44607846+zhangbiao1231@users.noreply.github.com>
2024-08-07 14:35:25 +0800

    Update dataloaders.py

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,15 +1,9 @@
 import torch
 import os
 import torchvision
-from pathlib import Path
-import sys
+
 from utils.augmentations import classify_augmentations
 
-FILE = Path('dataloaders.py').resolve()
-ROOT = FILE.parents[1]  # dog-breed root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))
 def dataSets(data_dir,folder,augment = True):
     augs = classify_augmentations(augment)
     return  torchvision.datasets.ImageFolder(
