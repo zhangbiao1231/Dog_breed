ca68039d0eb2295c7af90ece91a1d74a8aebad45
zhangbiao1231 <18856307989@163.com>
2024-08-12 19:06:09 +0800

    add LOGGING

--- utils/general.py
+++ utils/general.py
@@ -1,4 +1,41 @@
 import torch
+
+import os
+import logging
+import logging.config
+
+LOGGING_NAME = "dog-breed"
+
+def set_logging(name=LOGGING_NAME, verbose=True):
+    """Configures logging with specified verbosity; `name` sets the logger's name, `verbose` controls logging level."""
+    rank = int(os.getenv("RANK", -1))  # rank in world for Multi-GPU trainings
+    level = logging.INFO if verbose and rank in {-1, 0} else logging.ERROR
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {name: {"format": "%(message)s"}},
+            "handlers": {
+                name: {
+                    "class": "logging.StreamHandler",
+                    "formatter": name,
+                    "level": level,
+                }
+            },
+            "loggers": {
+                name: {
+                    "level": level,
+                    "handlers": [name],
+                    "propagate": False,
+                }
+            },
+        }
+    )
+
+
+set_logging(LOGGING_NAME)  # run before defining LOGGER
+LOGGER = logging.getLogger(LOGGING_NAME)  # define globally (used in train.py, val.py, detect.py, etc.)
+
 def try_gpu(i=0):  #@save
     """如果存在，则返回gpu(i)，否则返回cpu()"""
     if torch.cuda.device_count() >= i + 1:
